44c8eb1d709f7cb7db0d93a360d9917b39d9b725
Janette Huerta <janette.huerta0241@alumnos.udg.mx>
2020-10-05 00:11:00 -0500

    Terminado la actividad 07

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <string.h>
+
 
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+
+    
+    string valor;
+    cout <<"Ingrese tipo de dato: ";
+    getline(cin,valor);
+
+    if (valor == "int"){
+        cout <<sizeof(int)<<endl;
+    }
+    else if(valor == "char"){
+        cout <<sizeof(char)<<endl;
+    }
+    else if(valor == "short"){
+        cout <<sizeof(short)<<endl;
+    }
+    else if(valor == "float"){
+        cout <<sizeof(float)<<endl;
+    }
+    else if(valor == "double"){
+        cout <<sizeof(double)<<endl;
+    }
+    else if(valor == "long"){
+        cout <<sizeof(long)<<endl;
+    }
 
     return 0;
 }
